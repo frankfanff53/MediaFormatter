0c86ef8220482ebd699cf5cdf042c331eb7b4434
Feifan Fan <frankfanff53@gmail.com>
2023-04-24 16:40:28 +0100

    Update the error handling procedure in sub extraction.

--- extract.py
+++ extract.py
@@ -12,13 +12,20 @@ if __name__ == "__main__":
         "-d",
         "--directory",
         type=str,
-        default=".",
+        default='.',
         help="Directory to extract subtitles from",
     )
 
     args = parser.parse_args()
 
-    base_path = Path(os.getcwd()) / args.directory
+    base_path = Path(args.directory)
+    if not base_path.is_dir():
+        raise ValueError(f"{base_path} is not a directory.")
+        exit(1)
+    if not base_path.exists():
+        raise ValueError(f"{base_path} does not exist.")
+        exit(1)
+
     for file in base_path.iterdir():
         if file.is_file() and file.suffix == ".mkv":
             result = subprocess.run(
